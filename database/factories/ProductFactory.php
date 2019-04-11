732ab1a83f9c247b5e778a6fcb70f1c0d0ea007e
Sohail Khan <saytosohailkhan@gmail.com>
2019-04-11 20:01:54 +0500

    Database Seeded

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Product::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->word,
+        'description' => $faker->paragraph,
+        'price' => $faker->numberBetween(100,500),
+        'stock' => $faker->randomDigit,
+        'discount' => $faker->numberBetween(2,30),
     ];
 });
